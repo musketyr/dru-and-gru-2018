4457771d252e182baa6ca48c8989eaa5138ec236
Vladimir Orany <vladimir@orany.cz>
2018-05-31 06:42:08 +0200

    Micronaut exercise

--- hello-world/src/test/groovy/hello/world/GreetingsSpec.groovy
+++ hello-world/src/test/groovy/hello/world/GreetingsSpec.groovy
@@ -9,38 +9,43 @@ import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
 
-@Rollback
+@Rollback                                                                       // <1>
 class GreetingsSpec extends Specification {
 
-    @Shared @AutoCleanup EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
-    @Shared GreetingsService service = embeddedServer.applicationContext.getBean(GreetingsService)
+    @Shared @AutoCleanup
 
-    private static final List<Map<String, String>> GREETINGS = [
-            [language: 'en', greeting: 'Hello!'],
-            [language: 'cs', greeting: 'Ahoj!'],
+    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)      // <2>
+
+    @Shared GreetingsService service = embeddedServer.applicationContext
+                                                     .getBean(GreetingsService) // <3>
+
+    private static final List<Map<String, String>> GREETINGS = [                // <4>
+            [language: 'dk', greeting: 'Hej Verden!'],
+            [language: 'en', greeting: 'Hello World!'],
+            [language: 'es', greeting: 'Hola Mundo!'],
     ]
 
     @Rule Dru dru = Dru.plan {
-        from 'GREETINGS', {
+        from 'GREETINGS', {                                                     // <5>
             map {
-                to Greetings
+                to Greetings                                                    // <6>
             }
         }
     }
 
     void setup() {
-        dru.load()
+        dru.load()                                                              // <7>
     }
 
     void 'load with dru and fetch by gorm method'() {
         expect:
-            dru.findAllByType(Greetings).size() == 2
-            Greetings.findByLanguage('cs')
+            dru.findAllByType(Greetings).size() == 3                            // <8>
+            Greetings.findByLanguage('dk')                                      // <9>
     }
 
     void 'load with dru and fetch by gorm service'() {
         expect:
-            dru.findAllByType(Greetings).size() == 2
-            service.findByLanguage('cs')
+            dru.findAllByType(Greetings).size() == 3
+            service.findByLanguage('dk')                                        // <10>
     }
 }
--- hello-world/src/test/groovy/hello/world/HelloControllerSpec.groovy
+++ hello-world/src/test/groovy/hello/world/HelloControllerSpec.groovy
@@ -11,23 +11,24 @@ import spock.lang.Specification
 
 class HelloControllerSpec extends Specification {
 
-    @Shared @AutoCleanup EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
+    @Shared @AutoCleanup
+    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)      // <1>
 
-    @Rule Gru gru = Gru.equip(Http.steal(this))
+    @Rule Gru gru = Gru.equip(Http.steal(this))                                 // <2>
 
     void setup() {
-        String serverUrl = embeddedServer.getURL()
+        String serverUrl = embeddedServer.getURL()                              // <3>
         gru.prepare {
-            baseUri serverUrl
+            baseUri serverUrl                                                   // <4>
         }
     }
 
     void "test index"() {
         expect:
             gru.test {
-                get '/hello'
+                get '/hello'                                                    // <5>
                 expect {
-                    text inline('Hello World')
+                    text inline('Hello World')                                  // <6>
                 }
             }
     }
